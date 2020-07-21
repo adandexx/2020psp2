5f21275373f4fb13432366a8c942d693879731e8
adandexx <adasndexx.jp@gmail.com>
2020-07-21 21:02:44 +0900

    code changed

--- k03/k03.c
+++ k03/k03.c
@@ -19,10 +19,10 @@ int main(int argc, char* argv[])
     double sigma;
     int i;
     double dummy;
-
-
-
-
+    
+    sscanf(argv[argc - 3],"%lf",&mu);
+    sscanf(argv[argc - 2],"%lf",&sigma);
+    sscanf(argv[argc - 1],"%d",&num_dummy);
 
     printf("============================================\n");
     printf("template mean: %4.1lf\n",mu);
@@ -31,9 +31,10 @@ int main(int argc, char* argv[])
     printf("============================================\n");
 
     srand(RAND_SEED);
-    for(  ){
+
+    for(i=0;i<num_dummy;i++){
         /* r_stdnormを使って，1人のデータを捏造 */
-        dummy =   ;
+        dummy = r_stdnorm() * sigma + mu;
         printf("%5.2lf\n",dummy);
     }
 
